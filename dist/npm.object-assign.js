bc5b6f8816b851b493cd166b4ba4cd9c1c462cb3
therealDaze <danieldahlman@outlook.com>
2019-01-17 19:55:40 +0100

    updated babel-loader to 8 and installed scoped packages from babel (ex. @babel/preset-env)

--- dist/npm.object-assign.js
+++ dist/npm.object-assign.js
@@ -1,4 +1,4 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{21:function(r,t,e){"use strict";
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{54:function(r,t,e){"use strict";
 /*
 object-assign
 (c) Sindre Sorhus
