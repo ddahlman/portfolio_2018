07cae86dbb0cca4afc34901ae0d5cf182dda6711
therealDaze <danieldahlman@outlook.com>
2021-02-21 18:13:42 +0100

    Refactoring: Made AboutTools a functional component

--- src/components/About/AboutTools.js
+++ src/components/About/AboutTools.js
@@ -1,39 +1,35 @@
-import React, { Component } from 'react';
+import React from 'react';
 import styles from './About.scss';
 import AboutToolItems from './AboutToolItems';
 
-export default class AboutTools extends Component {
-    constructor() {
-        super();
-        this.state = {
-            items: [
-                { id: 1, header: "Version control", text: ["Git", "TFS"] },
-                {
-                    id: 2,
-                    header: "UX/UI webdesign tools",
-                    text: ["Invision", "Figma", "Adobe Photoshop"]
-                },
-                { id: 3, header: "Agile styles", text: ["Scrum", "Kanban"] },
+const AboutTools = () => {
 
-                {
-                    id: 4,
-                    header: "Techniques and paterns",
-                    text: [
-                        "Functional Programming",
-                        "Object Oriented Programming",
-                        "Higher Order Functions",
-                        "Ajax"
-                    ]
-                }
-            ]
-        };
-    }
-    render() {
-        const { items } = this.state;
-        return (
-            <ul className={styles.aboutToolsContainer}>
-                <AboutToolItems items={items} />
-            </ul>
-        );
-    }
-}
+	const items = [
+		{ id: 1, header: 'Version control', text: ['Git', 'TFS'] },
+		{
+			id: 2,
+			header: 'UX/UI webdesign tools',
+			text: ['Invision', 'Figma', 'Adobe Photoshop']
+		},
+		{ id: 3, header: 'Agile styles', text: ['Scrum', 'Kanban'] },
+
+		{
+			id: 4,
+			header: 'Techniques and paterns',
+			text: [
+				'Functional Programming',
+				'Object Oriented Programming',
+				'Higher Order Functions',
+				'Ajax'
+			]
+		}
+	];
+
+	return (
+		<ul className={styles.aboutToolsContainer}>
+			<AboutToolItems items={items} />
+		</ul>
+	);
+};
+
+export default AboutTools;
