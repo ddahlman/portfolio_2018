65b706d05f3ceed1a323c5cacb046bc13a0d9e2b
therealDaze <danieldahlman@outlook.com>
2021-02-21 18:08:07 +0100

    Refactoring: Added proptypes to AllBarColumns and BarColumn

--- src/components/About/AllBarColumns.js
+++ src/components/About/AllBarColumns.js
@@ -1,29 +1,36 @@
-import React from "react";
-import BarColumn from "./BarColumn";
-import style from "./About.scss";
+import React from 'react';
+import BarColumn from './BarColumn';
+import PropTypes from 'prop-types';
+
 
 const AllBarColumns = ({ columns, isGrowing }) => {
-    const bars = columns.reduce(
-        (acc, bar, i) => {
-            acc.components = [
-                ...acc.components,
-                <BarColumn
-                    key={bar.id}
-                    isGrowing={isGrowing}
-                    bar={bar}
-                    spaceX={(i + 1) * 15}
-                    pointX={acc.pointX}
-                    angleX={acc.angleX}
-                    skill={bar.skill}
-                />
-            ];
-            acc.pointX += 30;
-            acc.angleX += 30;
-            return acc;
-        },
-        { pointX: 17, angleX: 10, components: [] }
-    );
-    return bars.components;
+	const bars = columns.reduce(
+		(acc, bar, i) => {
+			acc.components = [
+				...acc.components,
+				<BarColumn
+					key={bar.id}
+					isGrowing={isGrowing}
+					bar={bar}
+					spaceX={(i + 1) * 15}
+					pointX={acc.pointX}
+					angleX={acc.angleX}
+					skill={bar.skill}
+				/>
+			];
+			acc.pointX += 30;
+			acc.angleX += 30;
+			return acc;
+		},
+		{ pointX: 17, angleX: 10, components: [] }
+	);
+	return bars.components;
 };
+
+AllBarColumns.propTypes = {
+	isGrowing: PropTypes.bool,
+	columns: PropTypes.array
+};
+
 
 export default AllBarColumns;
