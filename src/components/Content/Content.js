f1c236accd4058670f52eda2aac56548b1c39e46
ddahlman <danieldahlman@outlook.com>
2021-11-27 00:01:23 +0100

    Content: Trying out react lazy and Suspense

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,16 +1,27 @@
 import { Route, Switch } from 'react-router-dom';
-import Home from '../Home/Home';
-import About from '../About/About';
-import Portfolio from '../Portfolio/Portfolio';
+/* import {Home as HomePage} from '../Home/Home';
+import {About as AboutPage} from '../About/About';
+import {Portfolio as PortfolioPage} from '../Portfolio/Portfolio'; */
+import { lazy, Suspense } from 'react';
+
+const Home = lazy(() => import(/* webpackChunkName: "Home" */ '../Home/Home'));
+const About = lazy(() =>
+    import(/* webpackChunkName: "About" */ '../About/About')
+);
+const Portolio = lazy(() =>
+    import(/* webpackChunkName: "Portfolio" */ '../Portfolio/Portfolio')
+);
 
 const Content = () => {
     return (
-        <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/about" component={About} />
-            <Route path="/portfolio" component={Portfolio} />
-            <Route render={() => <h1>404 Page is not found</h1>} />
-        </Switch>
+        <Suspense fallback={<div>Loading yo</div>}>
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/about" component={About} />
+                <Route path="/portfolio" component={Portolio} />
+                <Route render={() => <h1>404 Page is not found</h1>} />
+            </Switch>
+        </Suspense>
     );
 };
 
